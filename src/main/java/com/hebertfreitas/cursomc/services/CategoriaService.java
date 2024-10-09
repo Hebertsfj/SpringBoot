788d38fadaae7006f10d9510815064e36a386f73
Hebertsfj <hebertsdefreitas@gmail.com>
2024-10-09 11:19:37 -0300

    Ajustes no Endpoit /categorias/id

--- src/main/java/com/hebertfreitas/cursomc/services/CategoriaService.java
+++ src/main/java/com/hebertfreitas/cursomc/services/CategoriaService.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.hebertfreitas.cursomc.domain.Categoria;
 import com.hebertfreitas.cursomc.repositories.CategoriaRepository;
 
@@ -14,6 +13,6 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new com.hebertfreitas.cursomc.services.exception.ObjectNotFoundException("Objeto não Encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
 	}
 }
