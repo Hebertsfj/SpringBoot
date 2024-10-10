c6a49eeaa54ab6d8be35b06121c27f5cf196f74c
Hebertsfj <hebertsdefreitas@gmail.com>
2024-10-10 16:09:31 -0300

    Item Pedido e Item PedidoPK

--- src/main/java/com/hebertfreitas/cursomc/CursomcApplication.java
+++ src/main/java/com/hebertfreitas/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.hebertfreitas.cursomc;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
@@ -13,6 +14,7 @@ import com.hebertfreitas.cursomc.domain.Cidade;
 import com.hebertfreitas.cursomc.domain.Cliente;
 import com.hebertfreitas.cursomc.domain.Endereco;
 import com.hebertfreitas.cursomc.domain.Estado;
+import com.hebertfreitas.cursomc.domain.ItemPedido;
 import com.hebertfreitas.cursomc.domain.Pagamento;
 import com.hebertfreitas.cursomc.domain.PagamentoComBoleto;
 import com.hebertfreitas.cursomc.domain.PagamentoComCartao;
@@ -25,6 +27,7 @@ import com.hebertfreitas.cursomc.repositories.CidadeRepository;
 import com.hebertfreitas.cursomc.repositories.ClienteRepository;
 import com.hebertfreitas.cursomc.repositories.EnderecoRepository;
 import com.hebertfreitas.cursomc.repositories.EstadoRepository;
+import com.hebertfreitas.cursomc.repositories.ItemPedidoRepository;
 import com.hebertfreitas.cursomc.repositories.PagamentoRepository;
 import com.hebertfreitas.cursomc.repositories.PedidoRepository;
 import com.hebertfreitas.cursomc.repositories.ProdutoRepository;
@@ -48,6 +51,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -110,6 +115,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 	
--- src/main/java/com/hebertfreitas/cursomc/domain/Pedido.java
+++ src/main/java/com/hebertfreitas/cursomc/domain/Pedido.java
@@ -1,8 +1,12 @@
 package com.hebertfreitas.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -11,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -33,6 +38,9 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Pedido() {
 		
 	}
@@ -43,6 +51,14 @@ public class Pedido implements Serializable{
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x :itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -84,6 +100,14 @@ public class Pedido implements Serializable{
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -101,5 +125,7 @@ public class Pedido implements Serializable{
 		Pedido other = (Pedido) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 }
