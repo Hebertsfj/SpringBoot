52f2f2da72182a0157a77af04c9fa291112849ab
Hebertsfj <hebertsdefreitas@gmail.com>
2024-10-17 15:47:40 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/hebertfreitas/cursomc/CursomcApplication.java
+++ src/main/java/com/hebertfreitas/cursomc/CursomcApplication.java
@@ -3,7 +3,6 @@ package com.hebertfreitas.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
